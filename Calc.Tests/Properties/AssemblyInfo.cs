d7deebb6cbe310230b3af29670efc75360c5f3f0
Danilov <albert95danilov@gmail.com>
2022-07-09 11:21:49 +0300

    Добавьте файлы проекта.

---
+++ Calc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calc.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calc.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a61d095-af30-4ece-b485-7da956eb54f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calc/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calculator
+    {
+        public double Div(double n1, double n2)
+        {
+            // Проверка деления на "0"
+            if (n2 == 0.0D)
+                throw new DivideByZeroException();
+            return n1 / n2;
+        }
+
+        public double AddWithInc(double n1, double n2)
+        {
+            return n1 + n2 + 1;
+        }
+    }
+}
